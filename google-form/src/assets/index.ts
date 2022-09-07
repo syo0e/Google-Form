b3b60acc81580d7545fc265b13c1a0b69517eb64
syo0e <kimdlzp@gmail.com>
2022-09-07 16:54:21 +0900

    [chore] icons 추가

---
+++ google-form/src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as AddIcon } from './icon/add';
+export { default as CopyIcon } from './icon/copy';
+export { default as ShowIcon } from './icon/show';
+export { default as TrashIcon } from './icon/trash';
